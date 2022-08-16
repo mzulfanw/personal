d93b6bccd22f7fb9675eec6c3acc90b13b276b9a
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2022-08-16 22:18:17 +0700

    feat: add configuration robotstxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://zulfan.netlify.app/',
+    // Important!
+    // Only official '@astrojs/*' integrations are currently supported by Astro.
+    // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
+    // with 'astro build' command.
+    experimental: {
+        integrations: true,
+    },
+    integrations: [robotsTxt()],
+});
